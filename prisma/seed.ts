f3d58e456ac5f0e47727b966a09d257543035b3f
Oleksii Nikolaienko <alex5272372@gmail.com>
2024-09-30 13:03:29 +0300

    Database structure and seed script

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,46 +1,96 @@
-import prisma from '../lib/prisma'
+import { PrismaClient } from '@prisma/client'
+import bcrypt from 'bcryptjs'
+import * as fs from 'fs'
+import * as path from 'path'
 
-async function main() {
-  const response = await Promise.all([
-    prisma.users.upsert({
-      where: { email: 'rauchg@vercel.com' },
-      update: {},
-      create: {
-        name: 'Guillermo Rauch',
-        email: 'rauchg@vercel.com',
-        image:
-          'https://images.ctfassets.net/e5382hct74si/2P1iOve0LZJRZWUzfXpi9r/9d4d27765764fb1ad7379d7cbe5f1043/ucxb4lHy_400x400.jpg',
-      },
-    }),
-    prisma.users.upsert({
-      where: { email: 'lee@vercel.com' },
-      update: {},
-      create: {
-        name: 'Lee Robinson',
-        email: 'lee@vercel.com',
-        image:
-          'https://images.ctfassets.net/e5382hct74si/4BtM41PDNrx4z1ml643tdc/7aa88bdde8b5b7809174ea5b764c80fa/adWRdqQ6_400x400.jpg',
-      },
-    }),
-    await prisma.users.upsert({
-      where: { email: 'stey@vercel.com' },
-      update: {},
-      create: {
-        name: 'Steven Tey',
-        email: 'stey@vercel.com',
-        image:
-          'https://images.ctfassets.net/e5382hct74si/4QEuVLNyZUg5X6X4cW4pVH/eb7cd219e21b29ae976277871cd5ca4b/profile.jpg',
-      },
-    }),
-  ])
-  console.log(response)
+import companies from './data/companies.json'
+import countries from './data/countries.json'
+import departments from './data/departments.json'
+import employees from './data/employees.json'
+import people from './data/people.json'
+import users from './data/users.json'
+
+const prisma = new PrismaClient()
+
+const crypt = (pass: string) => bcrypt.hashSync(pass, bcrypt.genSaltSync(10))
+
+const mapElement = (element: any): any => {
+  const resEl: Record<string, unknown> = {}
+
+  for (const key in element) {
+    if (key.substring(0, 1) === '_')
+      resEl[key.substring(1)] = eval(element[key] as string)
+
+    else if (key.substring(0, 1) === '/')
+      resEl[key.substring(1)] = fs.readFileSync(path.join(__dirname, ...element[key]), 'utf8')
+
+    else if (Array.isArray(element[key]))
+      resEl[key] = mapData(element[key])
+
+    else if (typeof element[key] === 'object' && element[key] !== null)
+      resEl[key] = mapElement(element[key])
+
+    else
+      resEl[key] = element[key]
+  }
+  return resEl
 }
+
+const mapData = (data: any[]): any[] => {
+  const resData = []
+
+  for (const element of data) {
+    if (Array.isArray(element))
+      resData.push(mapData(element))
+
+    else if (typeof element === 'object' && element !== null)
+      resData.push(mapElement(element))
+
+    else
+      resData.push(element)
+  }
+  return resData
+}
+
+const main = async () => {
+  const countriesData = mapData(countries)
+  for (const create of countriesData) await prisma.country.upsert({ where: { code: create.code }, update: {}, create })
+  console.log('Countries creation completed')
+
+  const companiesData = mapData(companies)
+  for (const create of companiesData) await prisma.company.upsert({ where: { name: create.name }, update: {}, create })
+  console.log('Companies creation completed')
+
+  const departmentsData = mapData(departments)
+  for (const create of departmentsData) await prisma.department.upsert({ where: { name_companyId: {
+    name: create.name,
+    companyId: 1
+  }}, update: {}, create })
+  console.log('Departments creation completed')
+
+  const peopleData = mapData(people)
+  for (const create of peopleData) await prisma.person.upsert({ where: { firstName_lastName_birthdate: {
+    firstName: create.firstName,
+    lastName: create.lastName,
+    birthdate: create.birthdate
+  }}, update: {}, create })
+  console.log('People creation completed')
+
+  const usersData = mapData(users)
+  for (const create of usersData) await prisma.user.upsert({ where: { email: create.email }, update: {}, create })
+  console.log('Users creation completed')
+
+  const employeesData = mapData(employees)
+  for (const create of employeesData) await prisma.epmloyee.upsert({ where: { departmentId_personId: {
+    departmentId: create.departmentId,
+    personId: create.personId
+  }}, update: {}, create })
+  console.log('Employees creation completed')
+}
+
 main()
-  .then(async () => {
-    await prisma.$disconnect()
-  })
-  .catch(async (e) => {
-    console.error(e)
-    await prisma.$disconnect()
+  .catch(error => {
+    console.error(error)
     process.exit(1)
   })
+  .finally(async () => await prisma.$disconnect())
