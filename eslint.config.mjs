d013aaff0f089917a2e141bc5453c3428de6424a
Oleksii Nikolaienko <alex5272372@gmail.com>
2025-03-24 21:42:17 +0200

    Update packages with major versions

---
+++ eslint.config.mjs
@@ -0,0 +1,32 @@
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+const eslintConfig = [
+  ...compat.config({
+    extends: ["next/core-web-vitals", "next/typescript"],
+    rules: {
+      indent: ['error', 2, { 'SwitchCase': 1 }],
+      'linebreak-style': ['error', 'unix'],
+      quotes: ['error', 'single'],
+      semi: ['error', 'never'],
+      'max-len': ['error', { code: 120, tabWidth: 2 }],
+      'no-trailing-spaces': 'error',
+      'no-tabs': 'error',
+      'no-multi-spaces': 'error',
+      'eol-last': 'error',
+      'comma-spacing': ['error', { 'before': false, 'after': true }],
+      'array-bracket-spacing': ['error', 'never'],
+      'object-curly-spacing': ['error', 'always', { 'arraysInObjects': true, 'objectsInObjects': false }]
+    }
+  }),
+];
+
+export default eslintConfig;
